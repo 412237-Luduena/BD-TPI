4f3744ef395bdc8161e6fa67bbab8948bd56465f
412237-Luduena <412237@tecnicatura.frc.utn.edu.ar>
2025-05-22 17:57:32 +0000

    Inicializador del Proyecto

---
+++ chatYMensajeria/src/main/java/tpiBd/chatYMensajeria/ChatYMensajeriaApplication.java
@@ -0,0 +1,13 @@
+package tpiBd.chatYMensajeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatYMensajeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatYMensajeriaApplication.class, args);
+	}
+
+}
